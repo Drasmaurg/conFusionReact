bf687f02509d18ba59be26fa4a619c0b619f9959
Drasmaurg <mauricionunez2011@gmail.com>
2022-04-23 10:41:30 -0500

    componentes part 2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import logo from './logo.svg';
 import { Nav, Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponents';
 import './App.css';
+import React, {Component} from 'react';
+import { DISHES } from './shared/dishes';
 
-function App() {
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
   return (
     <div>
       <Navbar dark color="primary">
@@ -11,9 +22,10 @@ function App() {
           <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
+  }
 }
 
 export default App;
