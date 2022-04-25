9d31481a3edb7a2b6591815b42fbd5a305f4c474
Drasmaurg <mauricionunez2011@gmail.com>
2022-04-25 17:15:56 -0500

    presentation and container components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import { Nav, Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponents';
 import './App.css';
-import { DISHES } from './shared/dishes';
-
+import Main from './components/MainComponent';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
 
   render() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className='container'>
-          <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} />
-    </div>
-  );
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
   }
 }
 
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,71 +1,68 @@
-import React, { Component } from 'react';  //Siempre importamos el componente de React a la hora de empezar a crear un componente
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
-
-    class DishDetail extends Component {
-
-            constructor(props) {
-            super(props);
-         }
-
-         renderDish(dish) {
-            return (
-               <div className="col-12 col-md-5 m-1">
-                  <Card>
-                     <CardImg top src={dish.image} alt={dish.name} />
-                     <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                     </CardBody>
-                  </Card>
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+    
+class Dishdetail extends Component {
+   
+   constructor(props) {
+      super(props);
+   }
+   
+   renderDish(dish) {
+      return (
+         <div className="col-12 col-md-5 m-1">
+            <Card>
+               <CardImg top src={dish.image} alt={dish.name} />
+               <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+               </CardBody>
+            </Card>
+         </div>
+      );
+   }
+   
+   renderComments(comments) {
+      if (comments != null) {
+         return (
+            <div className="col-12 col-md-5 m-1">
+               <h4>Comments</h4>
+               <ul className="list-unstyled">
+               {comments.map((comment) => {
+                  return (
+                     <li key={comment.id}>
+                       <p>{comment.comment}</p>
+                       <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>                                      
+                     </li>
+                  );
+                })}
+                </ul>
+            </div>
+         );
+      }
+      else {
+         return (
+            <div></div>
+         );
+      }
+   }
+   
+   render() {
+      if (this.props.dish != null) {
+         return (
+            <div className="container">
+               <div className="row">
+                  {this.renderDish(this.props.dish)}
+                  {this.renderComments(this.props.dish.comments)}
                </div>
-            );
-         }
+            </div>
+         );
+      }
+      else {
+         return (
+            <div></div> 
+         );
+      }
+   }
+}
 
-         renderComments() {
-             if (this.props.dish.comments == null) {
-                 return (
-                     <div className="container">
-
-                     </div>
-                 )
-             } else {
-                 return (
-                     <div className='col-12 col-md-5 m-1'>
-                         <div className='column'>
-                             <h4>Comments</h4>
-                             {this.props.dish.comments.map (
-                                 (value, idx) => {
-                                     return(
-                                         <div>
-                                             <p>{value.comment}</p>
-                                             <p>-- {value.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(value.date)))}</p> 
-                                         </div>
-                                     )
-                                 }
-                             )}
-                         </div>
-                     </div>
-                 );
-             }
-         }
-
-         render() {
-            if (this.props.dish != null) {
-               return (
-                  <div className="container">
-                     <div className="row">
-                        {this.renderDish(this.props.dish)}
-                        {this.renderComments(this.props.dish.comments)}
-                     </div>
-                  </div>
-               );
-            }
-            else {
-               return (
-                  <div></div>
-               );
-            }
-         }
-    }
-export default DishDetail; // Y al final del archivo siempre exportamos el componente creado para poder usarlo en otros archivos
+export default Dishdetail; // Y al final del archivo siempre exportamos el componente creado para poder usarlo en otros archivos
